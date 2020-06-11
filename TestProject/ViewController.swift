ff089553bb75d3b4d8b4a3f791cd033033de3ad3
solncevx1 <solncevx1@gmail.com>
2020-06-11 13:52:14 +0700

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Максим Солнцев on 6/11/20.
+//  Copyright © 2020 Максим Солнцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
